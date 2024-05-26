7859faa57656a9259190a6c7b2e8cd756eec1351
Juned-1 <jlaskar806@gmail.com>
2024-05-26 20:12:52 +0530

    refresh check 4

--- frontEnd/src/main.ts
+++ frontEnd/src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { RouteReuseStrategy} from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
@@ -19,7 +19,8 @@ import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    //{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
     provideAnimations(),
     provideAnimations(),

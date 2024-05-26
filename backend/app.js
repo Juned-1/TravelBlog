e4d27027abfc83ce4530dc00aa279a1191dd7df7
Juned-1 <jlaskar806@gmail.com>
2024-05-26 19:56:30 +0530

    refresh check 4

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser(cookieSecret));
 
 //testing route
-app.route("/*", (req, res) => {
+app.route("/*").get((req, res) => {
   return res.sendFile(path.join(__dirname,"..","frontEnd","www","index.html"));
 });
 app.use("/api/v1", appRouter);
